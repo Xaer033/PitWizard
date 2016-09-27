2fab01c572a871ddb71f8ae1144dadbce7eb3c6c
Julian Williams <jdfrag@gmail.com>
2016-09-27 11:54:41 -0700

    Add Renderstate + Shader definition files

---
+++ libraries/GhostwareEngine/src/Graphics/RenderState.cpp
@@ -0,0 +1,173 @@
+
+#include "RenderState.h"
+
+#include <vector>
+#include <unordered_map>
+
+
+#include <IwGL.h>
+#include <IwGLExt.h>
+
+#include <GG/Graphics/Shader.h>
+#include <GG/Graphics/Texture.h>
+//#include <FrameBufferObject.h>
+
+
+//#define BUFFER_OFFSET(i) ((void*)(i))
+
+
+namespace GG
+{
+	RenderState::RenderState()
+	{
+		_clearCache();
+	}
+
+	RenderState::~RenderState()
+	{
+		_clearCache();
+	}
+
+	void RenderState::clearRenderBuffer( int mode )
+	{
+		glClear( mode );
+	}
+
+	void RenderState::setClearColor( const Vector4 & clearColor ) const
+	{
+		glClearColor(clearColor.r, clearColor.g, clearColor.b, clearColor.a);
+	}
+
+	void RenderState::setDepthTesting( bool depthTest )
+	{
+		if( _cachedDepthTest == depthTest )
+			return;
+
+		if( depthTest )
+			glEnable( GL_DEPTH_TEST );
+		else
+			glDisable( GL_DEPTH_TEST );
+
+		_cachedDepthTest = depthTest;
+	}
+
+	void RenderState::setDepthRange( float zNear, float zFar )
+	{
+		glDepthRangef( zNear, zFar );
+	}
+
+	void RenderState::setCullMode( const CullMode cullMode )
+	{
+		if( _cachedCullMode == cullMode )
+			return;
+
+        if( cullMode == CULL_NONE )
+		{
+			glDisable( GL_CULL_FACE );
+		}
+		else
+		{
+			glEnable( GL_CULL_FACE );
+			glCullFace( (GLenum)cullMode );
+		}
+
+		_cachedCullMode = cullMode;
+
+	}
+
+	void RenderState::setBlendmode( const BlendMode mode )
+	{
+		if( _cachedBlendmode == mode )
+			return;
+
+		_cachedBlendmode = mode;
+
+
+        if( mode == BM_NONE )
+		{
+			glDisable( GL_BLEND );
+			return;
+		}
+				
+
+		glEnable( GL_BLEND );
+
+		switch( mode )
+		{
+            case BM_ALPHA:		glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );	break;
+			case BM_ADDITIVE:	glBlendFunc( GL_SRC_ALPHA, GL_ONE );					break;
+            case BM_MULTIPLY:	glBlendFunc( GL_DST_COLOR, GL_ONE_MINUS_SRC_ALPHA );	break;
+            case BM_SCREEN:		glBlendFunc( GL_ONE, GL_ONE_MINUS_SRC_COLOR );			break;
+		}
+	}
+
+
+	void RenderState::setRenderSize( int width, int height )
+	{
+		_renderWidth	= width;
+		_renderHeight	= height;
+	}
+
+	void RenderState::setViewport(const Vector4 & viewport)
+	{
+		Vector2 xVec = Vector2(viewport.x, viewport.z) * (float)_renderWidth;
+		Vector2 yVec = Vector2(viewport.y, viewport.w) * (float)_renderHeight;
+
+		glViewport((GLint)xVec.x, (GLint)yVec.x, (GLsizei)xVec.y, (GLsizei)yVec.y );
+	}
+
+
+
+	void RenderState::bindTexture2d( uint samplerID, const Texture & texture )
+	{
+		TextureCache::iterator it =  _cachedTextures.find( samplerID );
+
+		uint32 texHandle = texture.m_HWID;
+
+		if( it == _cachedTextures.end() )
+			_setTexture( samplerID, texHandle );
+		else if( it->second != texHandle )
+			_setTexture( samplerID, texHandle );
+	}
+
+
+	void RenderState::bindShader( const Shader & shader )
+	{
+		uint shaderId = shader.getId();
+
+		if( shaderId == _cachedShader )
+			return;
+
+		glUseProgram( shaderId );
+
+		_cachedShader = shaderId;
+	}
+
+
+	//void RenderState::bindFrameBuffer( const uint & frameBuffer )
+	//{
+	//	glBindFramebuffer( GL_FRAMEBUFFER, frameBuffer );
+	//}
+
+	void RenderState::_clearCache()
+	{
+		_cachedShader		= 0;
+
+		_cachedFrameBuffer	= 0;
+
+		_cachedCullMode		= CULL_BACK;
+
+		_renderWidth		= IwGLGetInt( IW_GL_WIDTH );
+		_renderHeight		= IwGLGetInt( IW_GL_HEIGHT );
+
+		_cachedTextures.clear();
+	}
+
+	void RenderState::_setTexture( uint samplerId, uint texHandle )
+	{
+		glActiveTexture( GL_TEXTURE0 + samplerId );
+		glBindTexture( GL_TEXTURE_2D, texHandle );
+
+		_cachedTextures[ samplerId ] = texHandle;
+	}
+}
---
+++ libraries/GhostwareEngine/src/Graphics/Shader.cpp
@@ -0,0 +1,282 @@
+
+#include "Shader.h"
+
+
+#include <map>
+#include <sstream>
+#include <vector>
+#include <string>
+
+
+#include "s3e.h"
+#include "IwGL.h"
+
+#include <GG/Core/Vector.h>
+#include <GG/Core/Matrix.h>
+#include <GG/Core/Log.h>
+#include <GG/Graphics/Texture.h>
+
+
+namespace GG
+{
+	Shader::Shader()
+	{
+		vertexHandle	= 0;
+		pixelHandle		= 0;
+
+		programHandle	= 0;
+
+		for( int i = 0; i < 7; ++i )
+			attributeHandles.push_back("");
+
+	}
+
+	Shader::~Shader()
+	{
+		if( vertexHandle )
+			glDeleteShader( vertexHandle );
+
+		if( pixelHandle )
+			glDeleteShader( pixelHandle );
+
+		if( programHandle )
+			glDeleteProgram( programHandle );
+	}
+
+
+	uint Shader::getId() const
+	{
+		return programHandle;
+	}
+
+	void Shader::bindAttribute( int tag, const std::string & name )
+	{
+		if( (uint)tag < attributeHandles.size() )
+			attributeHandles[ tag ] = name;
+	}
+
+	void Shader::bind( )
+	{
+		glUseProgram( programHandle );
+	}
+
+
+// Create a shader object, load the shader source, and
+// compile the shader.
+	void Shader::_validateProgram( )
+	{
+		const uint BUFFER_SIZE = 1024;
+		char buffer[ BUFFER_SIZE ];
+		memset( buffer, 0, BUFFER_SIZE );
+
+		GLsizei length = 0;
+		memset( buffer, 0, BUFFER_SIZE );
+
+		glGetProgramInfoLog( programHandle, BUFFER_SIZE, &length, buffer);
+
+		if (length > 0)
+		{
+			LOG_ERROR("Program %d linker: %s", programHandle, buffer);
+		}
+
+		glValidateProgram( programHandle );
+
+		GLint status = 0;
+
+		glGetProgramiv( programHandle, GL_VALIDATE_STATUS, &status );
+		if (status == GL_FALSE)
+		{
+			LOG_ERROR("Error validating shader!");
+		}
+	}
+
+
+	bool Shader::loadShader( GLuint & shader, const std::string & shaderSrc, GLenum type )
+	{
+
+		if( shaderSrc.empty() ) { return false; }
+
+		GLint compiled;
+		// Create the shader object
+        if( shader == 0 )
+			shader = glCreateShader( type );
+
+		std::stringstream stream;
+		for(int i = 0; i < defineList.size(); ++i)
+		{
+			stream << defineList[i];
+		}
+		stream << shaderSrc;
+		const char * shaderString = stream.str().c_str();
+		//TODO: get char array from vector of strings
+		glShaderSource( shader, 1, &shaderString, NULL );	
+
+		// Compile the shader
+		glCompileShader( shader );
+		// Check the compile status
+		glGetShaderiv( shader, GL_COMPILE_STATUS, &compiled );
+
+		if( !compiled )
+		{
+			GLint infoLen = 0;
+			glGetShaderiv( shader, GL_INFO_LOG_LENGTH, &infoLen );
+
+			if( infoLen > 1 )
+			{
+				char * infoLog = (char*)malloc( sizeof(char) * infoLen );
+				glGetShaderInfoLog(shader, infoLen, NULL, infoLog);
+					
+				LOG_ERROR("Error Compiling shader: %s", infoLog);
+
+				free( infoLog );
+			}
+
+			glDeleteShader( shader );
+			return false;
+		}
+        return true;
+	}
+
+
+	void Shader::_bindAllAttributes()
+	{
+		for( uint i = 0; i < attributeHandles.size(); ++i )
+			glBindAttribLocation( programHandle, i, attributeHandles[i].c_str() );
+	}
+
+
+	void  Shader::addDefinition( const std::string & define )
+	{
+		defineList.push_back( define );
+	}
+
+
+	bool  Shader::compile(
+		const std::string & vertexShader, 
+		const std::string & pixelShader )
+	{
+		int linked	= 0;
+
+		bool vertSuccess	= loadShader( vertexHandle,  vertexShader,   GL_VERTEX_SHADER    );
+		bool pixelSuccess	= loadShader( pixelHandle,   pixelShader,    GL_FRAGMENT_SHADER  );
+			
+		if( !vertSuccess || !pixelSuccess ) 
+		{
+			LOG_ERROR("Vertex or Pixel shader did not load properly!");
+			return false; 
+		}
+
+		if( programHandle == 0 )
+        { 
+			programHandle	= glCreateProgram();
+        }
+
+		glAttachShader( programHandle, vertexHandle );
+		glAttachShader( programHandle, pixelHandle );
+
+		_bindAllAttributes();
+
+		// Link the program
+		glLinkProgram( programHandle );
+		// Check the link status
+		glGetProgramiv( programHandle, GL_LINK_STATUS, &linked );
+
+		if( !linked )
+		{
+			GLint infoLen = 0;
+			glGetProgramiv( programHandle, GL_INFO_LOG_LENGTH, &infoLen );
+
+			if( infoLen > 1 )
+			{
+				char* infoLog = (char*)malloc(sizeof(char) * infoLen);
+				glGetProgramInfoLog( programHandle, infoLen, NULL, infoLog );
+
+				LOG_ERROR("Error Compiling shader: %s", infoLog);
+				free( infoLog );
+			}
+			
+			glDeleteProgram( programHandle );
+			return false;
+		}
+
+			
+		_validateProgram();
+
+		return true;
+	}
+
+	void Shader::update( )
+	{
+
+	}
+
+		
+	int Shader::_getParameterId( const std::string & name )
+	{
+		if( programHandle == 0 ) { return -1; }
+
+		auto it = parameterHandles.find(  name  );
+		if( it == parameterHandles.end() )
+		{
+			int ID = glGetUniformLocation( programHandle, name.c_str() );
+			parameterHandles[ name ] = ID;
+
+			return ID;
+		}
+
+		return it->second;
+	}
+
+	void Shader::setParameter( const std::string & name, int x )
+	{
+		glUniform1i( _getParameterId( name ), x );
+	}
+
+	void Shader::setParameter(const std::string & name, float x )
+	{
+		glUniform1f( _getParameterId( name ), x );
+	}
+
+	void Shader::setParameter( const std::string & name, float x, float y )
+	{
+		glUniform2f( _getParameterId( name ), x, y );
+	}
+
+	void Shader::setParameter( const std::string & name, float x, float y, float z )
+	{
+		glUniform3f( _getParameterId( name ), x, y, z );
+	}
+
+	void Shader::setParameter( const std::string & name, float x, float y, float z, float w )
+	{
+		glUniform4f( _getParameterId(name), x, y, z, w );
+	}
+
+
+	void Shader::setParameter( const std::string & name, const Vector2 & v )
+	{
+		glUniform2f( _getParameterId( name ), v.x, v.y );
+	}
+
+	void Shader::setParameter( const std::string & name, const Vector3 & v )
+	{
+		glUniform3f( _getParameterId( name ), v.x, v.y, v.z );
+	}
+
+	void Shader::setParameter( const std::string & name, const Vector4 & v )
+	{
+		glUniform4f( _getParameterId( name ), v.x, v.y, v.z, v.w );
+	}
+
+	void Shader::setParameterSampler( const std::string & name, uint samplerID )
+	{
+	// Set the sampler texture unit to 0
+		glUniform1i( _getParameterId( name ) , samplerID );
+	}
+
+
+	void Shader::setParameter( const std::string & name, const Matrix4 & m )
+	{
+		glUniformMatrix4fv( _getParameterId( name ), 1, GL_FALSE, glm::value_ptr( m ) );
+	}
+}
