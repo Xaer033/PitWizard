965e3163108ecef55089bf43eea7abaef93553f2
Julian Williams <jdfrag@gmail.com>
2016-09-28 00:55:43 -0700

    Begin replacing material construct

--- libraries/GhostwareEngine/include/GG/Graphics/Material.h
+++ libraries/GhostwareEngine/include/GG/Graphics/Material.h
@@ -1,9 +1,24 @@
 // Ghostware Games inc. 2016  -Julian Williams
 #pragma once 
 
+#include <GG/Graphics/RenderState.h>
 #include "IwMaterial.h"
 
 namespace GG
 {
 	typedef CIwMaterial Material;
+
+	struct nMaterial
+	{
+	public:
+		BlendMode	blendMode;
+		ClearMode	clearMode;
+		CullMode	cullMode;
+
+		bool		isDepthTesting;
+		Vector2		depthRange;
+
+		uint32		shaderId;
+
+	};
 }
