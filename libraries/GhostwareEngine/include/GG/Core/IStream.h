a7a920da027a131a1bbf585231f00c18521cfc13
Julian Williams <jdfrag@gmail.com>
2016-10-24 18:23:29 -0700

    Add Stream interface

---
+++ libraries/GhostwareEngine/include/GG/Core/IStream.h
@@ -0,0 +1,19 @@
+// Ghostware Games inc. 2016  -Julian Williams
+#pragma once
+
+#include "Types.h"
+
+namespace GG
+{
+
+	class IStream
+	{
+	public:
+		virtual ~IStream() {}
+
+		virtual int64	read(void* buffer, uint32 size, uint32 count)	= 0;
+		virtual int64	write(const void* buffer, uint32 size, uint32 count) = 0;
+		virtual int64	seek(uint64 position) = 0;
+		virtual uint64	getSize() const	= 0;
+	};
+}
