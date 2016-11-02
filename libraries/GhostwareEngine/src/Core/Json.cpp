c0e93a058ff094f7b9a161df56362dbd7ecce89d
Julian Williams <jdfrag@gmail.com>
2016-11-01 22:10:33 -0700

    Log json file parsing errors

--- libraries/GhostwareEngine/src/Core/Json.cpp
+++ libraries/GhostwareEngine/src/Core/Json.cpp
@@ -2,42 +2,42 @@
 #include "Json.h"
 #include <string>
 #include <sstream>
-#include <fstream>
+#include <GG/Core/FileStream.h>
+#include <GG/Core/StreamHelper.h>
+#include <GG/Core/Log.h>
 
 namespace GG
 {
 	json JsonFromString(const std::string & str)
 	{
-		std::stringstream buffer;
-		buffer << str;
-
 		Json::CharReaderBuilder builder;
 		builder["collectComments"]	= true;
 		builder["allowComments"]	= true;
 		builder["allowNumericKeys"]	= true;
 
+		std::stringstream buffer;
+		buffer << str;
+
 		json value = json::null;
 		std::string errs;
+		const char * cBuff = str.c_str();
 		bool ok = Json::parseFromStream(builder, buffer, &value, &errs);
-
+		if(!ok)
+		{
+			TRACE_ERROR("Json Error: %s", errs);
+			return json::nullRef;
+		}
 		return value;
 	}
 
 	json JsonFromFile( const std::string & filename )
 	{
-		std::ifstream file( filename );
-		std::stringstream buffer;
-		buffer << file.rdbuf();
-
-		Json::CharReaderBuilder builder;
-		builder[ "collectComments" ]	= true;
-		builder[ "allowComments" ]		= true;
-		builder[ "allowNumericKeys" ]	= true;
-
-		json value = json::null;
-		std::string errs;
-		bool ok = Json::parseFromStream( builder, buffer, &value, &errs );
-
-		return value;
+		FileStream stream(filename, OpenMode::OPEN_READ);
+		if(!stream.isOpen())
+		{
+			TRACE_WARNING("Could not open json file from: %s", filename);
+			return json::nullRef;
+		}
+		return JsonFromString(Stream::ReadStringFromStream(&stream));
 	}
 }
